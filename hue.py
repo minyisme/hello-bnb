96e3eeeca6a4559ce7bda7538ccf0488c96038f6
hollywoodno <hollywoodno@gmail.com>
2016-07-16 09:12:57 -0700

    creating light class

--- hue.py
+++ hue.py
@@ -52,17 +52,39 @@ class HueControls(object):
 
         lights = requests.get("http://10.10.30.175/api/" + self.username + "/lights")
         
+        # should I loop over each light and create a seperate object class
+        # with a num attribute and a light toggle method?
         return lights.json()
 
+# class Light(HueControls):
+    # """ Creates a single light object for each bridge light to control """
+
+    # def __init__(self, num):
+    #     self.num = num
+        
+# consider seperating this to a new class
     def light_on(self, num): 
         """ Turns on a light based on the light number provided """
 
-        # set light state to on
-        payload = {"on": True}
 
+
+        light_state = requests.get("http://10.10.30.175/api/" + self.username + "/lights/" + str(num) + "/state")
         # make put request to update light state to on
-        light = requests.put("http://10.10.30.175/api/" + self.username + "/lights/" + str(num) + "/state", data=json.dumps(payload))
+
         resp = light.json()
+
+        # if light is off, put request to turn it on,
+        # otherwise put request to turn it off
+        if resp.state == "off":
+             # set light state to on
+            payload = {"on": True}
+            light = requests.put("http://10.10.30.175/api/" + self.username + "/lights/" + str(num) + "/state", data=json.dumps(payload))
+        else:
+            # set light state to on
+            payload = {"on": False}
+            light = requests.put("http://10.10.30.175/api/" + self.username + "/lights/" + str(num) + "/state", data=json.dumps(payload))
+
+        # resp = light.json()
         
         return resp
 
@@ -77,6 +99,8 @@ class HueControls(object):
         resp = light.json()
         
         return resp
+
+
 
 if __name__ == "__main__":
     pp = pprint.PrettyPrinter()
