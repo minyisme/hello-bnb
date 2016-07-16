a6f0a223d1504ef53a38dd3e9587f222faa6a95e
Maggie Yang <minyisme@gmail.com>
2016-07-15 21:27:11 -0700

    edited libs.py for add listings function

--- libs.py
+++ libs.py
@@ -1,5 +1,14 @@
 '''All the library things'''
 from model import Listing, Video
 
-def add_video():
-	pass
+def add_listing:
+    listing_0 = Listing(listing_url="https://www.airbnb.com/rooms/3060662", airbnb_id="nija.mashruwala@gmail.com")
+    db.session.add(listing_0)
+
+    listing_1 = Listing(listing_url="https://www.airbnb.com/rooms/2386522", airbnb_id="nija.mashruwala@gmail.com")
+    db.session.add(listing_1)
+
+    db.session.commit()
+
+    print "Listings successfully added."
+	
