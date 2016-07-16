be3043a2e74c2cae6dca8b72324d8b42591e43e6
Maggie Yang <minyisme@gmail.com>
2016-07-15 19:59:07 -0700

    models added, gitignore added

--- model.py
+++ model.py
@@ -13,6 +13,35 @@ db = SQLAlchemy()
 
 # All models go here
 
+class Listing(db.Model):
+    """Airbnb listings"""
+
+    __tablename__ = "listings"
+
+    listing_url = db.Column(db.String(1024), nullable=False, primary_key=True)
+    airbnb_id = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+
+        return ("<Listing listing_url=%s airbnb_id=%s>" %(self.listing_url, self.airbnb_id))
+
+
+
+class Video(db.Model):
+    """Listing videos"""
+
+    __tablename__ = "videos"
+
+    video_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    video_url = db.Column(db.String(1024), nullable=False)
+    listing_url = db.Column(db.String(1024), db.ForeignKey('listings.listing_url'), nullable=False)
+
+    listing = db.relationship('Listing', backref='video')
+
+    def __repr__(self):
+
+        return ("<Video video_id=%s video_url=%s listing_url=%s>" %(self.video_id, self.video_url, self.listing_url))
+
 
 if __name__ == "__main__":
     print "Successful Connected to database"
